6b02f46705fdcad65268d761281d8437afdfb8a5
r4v37t <r4v37t@gmail.com>
2015-07-09 16:08:27 +0700

    init commit

---
+++ model/db/db.config.php
@@ -0,0 +1,11 @@
+<?php
+$db['host']='localhost';
+$db['database']='af';
+$db['username']='root';
+$db['password']='';
+$db['charset']='utf8';
+$db['errmode']=PDO::ERRMODE_EXCEPTION;
+// $db['errmode']=PDO::ERRMODE_SILENT;
+// $db['errmode']=PDO::ERRMODE_WARNING;
+$db['emulate_prepare']=false;
+?>
